141fe296d5228582fa60d739100807fed0f87057
celerno@gmail.com <celerno@gmail.com>
2019-06-26 02:25:16 +0000

    vhosts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var connect = require('connect');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -8,19 +9,25 @@ var mongoose = require('mongoose');
 var db     = require('./db');
 var post_model = mongoose.model('cmd');
 var routes = require('./routes/index');
+var vhost = require('vhost');
 var app = express();
+var middle = connect();
+var serveStatic = require('serve-static');
+//use vhosts
+app.use(vhost('test.coolbox.com.mx', serveStatic('../coolbox.com.mx-Test/')));
+app.use(vhost('coolbox.com.mx', serveStatic('../coolbox.com.mx/')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(function (req, res, next) {
+middle.use(vhost('*.chamizo.pro', function (req, res, next) {
 	if(!req.secure){
 		res.redirect(301,'https://' + req.headers.host + req.url);
 	}
 	next();
-}); 
+})); 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,35 +38,35 @@ app.use('/', routes);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(vhost('chamizo.pro', function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
+}));
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(vhost('chamizo.pro', function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
       error: err
     });
-  });
+  }));
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(vhost('chamizo.pro', function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
   });
-});
+}));
 
 
 module.exports = app;
